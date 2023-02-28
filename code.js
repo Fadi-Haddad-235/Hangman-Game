3dfa74b865519fde1edd945e04837a8cb44aefe3
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-02-28 06:51:16 +0200

    added logic to replace chosen word with underscores

--- code.js
+++ code.js
@@ -12,10 +12,12 @@ window.onload=function(){
     let random_cat_value = words[random_cat_name];
 
     let random_value_number = Math.floor(Math.random() * random_cat_value.length)
-    let random_value_value = random_cat_value[random_value_number];
-    // console.log(random_value_value);
-    let random_word_letters = Array.from(random_value_value)
-
+    let random_word = random_cat_value[random_value_number];
+    let random_word_letters = Array.from(random_word.toLowerCase())
+    let under_scores = "_".repeat(random_word_letters.length)
+    const dotted_word = document.getElementById("word");
+    dotted_word.innerHTML = under_scores
+    console.log(dotted_word.innerHTML)
 
 
 document.addEventListener("click",(e) => {
@@ -24,7 +26,10 @@ document.addEventListener("click",(e) => {
     }
     let clickedLetter = e.target.innerHTML.toLowerCase()
     random_word_letters.forEach((word_letter, index)=>{
-        
+        if(clickedLetter == word_letter){
+            console.log(under_scores);
+
+        }
     })
 })
 }
